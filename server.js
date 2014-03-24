384a782c013e866311c6444ebf01a86a1dfd7404
David Blurton <david.blurton@gmail.com>
2014-03-24 17:34:24 +0000

    Add compress middleware

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   
+  app.use(express.compress());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/dist'));

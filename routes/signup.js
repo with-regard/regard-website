4bd52afabf3a652bf837ff409a4ffe3d1a997ac7
Neil Anderson <neil.t.anderson@gmail.com>
2014-03-25 22:51:56 +0000

    use mailing list api

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,25 @@
+var request = require('request');
+
 exports.sendToMailchimp = function (req, res) {
   if(req.body.forbots){
     console.log('Probably a bot ' +  req.body.email);
     res.redirect('/');
-  }
-  
-  var email = req.body.email;
-  // Send to mailchimp api
-  
+  } 
+
+  var signupData = {
+    email : {
+        email : req.body.email
+    },
+    apikey : process.env.MAILING_LIST_KEY,
+    id : process.env.MAILING_LIST_ID,
+    double_optin : false,
+    send_welcome : true,
+    email_type : 'html'
+  };
+
+  request.post(process.env.LIST_SUBSCRIBE_URL, {form: signupData }, function (error, response, body) {      
+            console.log(body);
+  });
+ 
   res.render('index', {signedUp: true}); // use flash here instead
 }

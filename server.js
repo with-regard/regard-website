bb7fe99bb00d5e6e4682e42e18628a3f8c831f06
David Blurton <david.blurton@gmail.com>
2014-03-25 10:23:55 +0000

    Formatting

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 var express = require('express'),
-    routes = require('./routes');
+  routes = require('./routes');
 
 var app = express();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  
+
   app.use(express.compress());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/dist'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function () {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+  app.use(express.errorHandler());
 });
 
 // Routes

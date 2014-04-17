c1b0034d5384fde72b4bc9e97dd06ded315834ff
David Blurton <david.blurton@gmail.com>
2014-04-17 18:31:24 +0100

    This file isn't used anywhere

--- modules/database.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));

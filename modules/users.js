484b57c51c216c741ee64cb9224666abeeece4a2
David Blurton <david.blurton@gmail.com>
2014-04-17 17:31:26 +0100

    Remove unnecessary function

--- modules/users.js
+++ modules/users.js
@@ -4,16 +4,14 @@ var controller = require('./userController.js');
 
 exports.findOrCreateUser = function (accessToken, refreshToken, profile) {
   // Try to find a matching user first
+  
   return controller.fetchUserByGithubId({
     githubId: profile.id
   }).then(function (user) {
     if (user) {
       return user;
     } else {
-      // else create a new profile for them
-      controller.createNewUserFromGithub(profile, function (err, user) {
-        return user;
-      });
+      return controller.createNewUserFromGithub(profile);
     }
   });
 };

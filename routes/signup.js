bdb83fb5c5ff8ef74ed045957846a12345a02269
David Blurton <david.blurton@gmail.com>
2014-04-21 23:32:00 +0100

    Formatting

--- routes/signup.js
+++ routes/signup.js
@@ -1,25 +1,27 @@
 var request = require('request');
 
 exports.sendToMailchimp = function (req, res) {
-  if(req.body.forbots){
-    console.log('Probably a bot ' +  req.body.email);
+  if (req.body.forbots) {
+    console.log('Probably a bot ' + req.body.email);
     res.redirect('/');
-  } 
+  }
 
   var signupData = {
-    email : {
-        email : req.body.email
+    email: {
+      email: req.body.email
     },
-    apikey : process.env.MAILING_LIST_KEY,
-    id : process.env.MAILING_LIST_ID,
-    double_optin : false,
-    send_welcome : true
+    apikey: process.env.MAILING_LIST_KEY,
+    id: process.env.MAILING_LIST_ID,
+    double_optin: false,
+    send_welcome: true
   };
 
-  request.post(process.env.LIST_SUBSCRIBE_URL, {form: signupData }, function (error, response, body) {      
-            console.log(body);
+  request.post(process.env.LIST_SUBSCRIBE_URL, {
+    form: signupData
+  }, function (error, response, body) {
+    console.log(body);
   });
- 
+
   req.flash('message', 'Thanks for signing up!');
   res.redirect('/');
-}
+};

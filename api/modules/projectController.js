c559483b325f595c04caa57caa0b76cd2a849530
David Blurton <david.blurton@gmail.com>
2014-04-25 19:33:38 +0100

    Make sure users can only access projects that belong to them

--- api/modules/projectController.js
+++ api/modules/projectController.js
@@ -2,36 +2,47 @@
 
 var express = require('express');
 var Project = require('../schemas/project.js');
+var User = require('../../schemas/userSchema.js');
 
 var app = express();
 
 app.get('/projects/:id', function (req, res, next) {
-  Project.findById(req.params.id).exec().then(function (project) {
-    if(!project){
-      res.send(404);
+  // Check if this user can access this project
+  User.findById(req.user._id).where('project_ids').in([req.params.id]).exec().then(function (allowed) {
+    if (!allowed) {
+      res.send(401);
+    } else {
+      Project.findById(req.params.id).exec().then(function (project) {
+        if (!project) {
+          res.send(404);
+        }
+        res.json({
+          "project": project
+        });
+      }, next);
     }
-    
-    res.json({
-      "project": project
-    });
   }, next);
 });
 
 app.get('/projects', function (req, res, next) {
-  // filter by user
-
-  Project.find().exec().then(function (projects) {
-    res.json({
-      "projects": projects
-    });
-  }, next);
+  User.findById(req.user._id).exec().then(function (user) {
+    Project.find({
+      '_id': {
+        $in: user.project_ids
+      }
+    }).exec().then(function (projects) {
+      res.json({
+        "projects": projects
+      });
+    }, next);
+  })
 });
 
 app.put('/projects/:id', function (req, res, next) {
   Project.findById(req.params.id).exec().then(function (project) {
     project.name = req.body.project.name;
     project.save();
-    
+
     res.json({
       "project": project
     });
@@ -44,22 +55,33 @@ app.post('/projects', function (req, res, next) {
   });
 
   Project.create(project).then(function () {
-    res.json({
-      "project": project
-    });
-  }, next);
-
-  // add project_id to user
+    User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        project_ids: project._id
+      }
+    }, {
+      safe: true,
+      upsert: true
+    }).exec().then(function () {
+      res.json({
+        "project": project
+      });
+    }, next);
+  });
 });
 
 app.delete('/projects/:id', function (req, res, next) {
   Project.findById(req.params.id).exec().then(function (project) {
     project.remove().exec().then(function () {
-      res.send('');
+      User.findByIdAndUpdate(req.user._id, {
+        $pull: {
+          project_ids: project._id
+        }
+      }).exec().then(function () {
+        res.send(200);
+      }, next);
     }, next)
   }, next);
-
-  // remove project_id from user
 });
 
 module.exports = app;

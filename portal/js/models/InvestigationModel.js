b20728dbef7596f02b2a62f44027b19d173d3b5c
David Blurton <david.blurton@gmail.com>
2014-04-29 18:14:25 +0100

    Add relationships to models

--- portal/js/models/InvestigationModel.js
+++ portal/js/models/InvestigationModel.js
@@ -1,3 +1,4 @@
 App.Investigation = DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string', {defaultValue: 'New investigation'}),
+  project: DS.belongsTo('project')
 });
--- portal/js/models/ProjectModel.js
+++ portal/js/models/ProjectModel.js
@@ -1,4 +1,4 @@
 App.Project = DS.Model.extend({
-  name: DS.attr('string', {defaultValue: 'My new project'}),
-  investigations: DS.hasMany('investigation')
+  name: DS.attr('string', {defaultValue: 'New project'}),
+  investigations: DS.hasMany('investigation', {embedded: 'always'})
 });

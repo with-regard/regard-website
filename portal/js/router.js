e42b027babb0452a2508f68b1f597e0ce20bf40e
David Blurton <david.blurton@gmail.com>
2014-05-01 09:06:24 +0100

    Fix templates

--- portal/js/router.js
+++ portal/js/router.js
@@ -1,10 +1,10 @@
 App.Router.map(function () {
   this.resource('projects', {path: '/'}, function() {
     this.resource('project', {path: 'projects/:project_id'}, function() {
-      this.resource('investigations', function() {
+      this.resource('investigations', {path: '/investigations'}, function() {
         this.resource('investigation', {path: '/:investigation_id'});  
       });
-    });    
+    });
   });
 });
 
@@ -28,11 +28,6 @@ App.ProjectsRoute = Ember.Route.extend({
   model: function () {
     return this.store.all('project');
   },
-  afterModel: function(projects, transition) {
-    if(!Ember.isEmpty(projects)){
-      this.transitionTo('project', projects.objectAt(0));  
-    }
-  },
   renderTemplate: function() {
     this.render('projects', { 
       outlet: 'off-canvas' 
@@ -48,7 +43,7 @@ App.ProjectRoute = Ember.Route.extend({
     project.get('investigations');
   },
   renderTemplate: function() {
-    this.render({
+    this.render('investigations', {
       outlet: 'main'
     });
     this.render('project-title', {

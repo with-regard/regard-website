e02083e3430645b3d6aa8ef47e09b4cf17df296a
David Blurton <david.blurton@gmail.com>
2014-05-08 15:38:44 +0100

    Auth is now an npm module

--- server.js
+++ server.js
@@ -4,29 +4,21 @@ var express = require('express');
 var compress = require('compression')();
 var requireSSLinProduction = require('./modules/requireSSLinProduction.js');
 var bodyParser = require('body-parser')();
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
 var errorHandler = require('errorhandler');
 
-var auth = require('./api/modules/auth.js');
+var regardUserStore = require('./modules/regard-user-store.js');
+var auth = require('regard-authentication');
 var api = require('./api/server.js');
 var portal = require('./portal/server.js');
 var website = require('./website/server.js');
 
 var app = express();
+
 app.use(compress);
 app.use(requireSSLinProduction);
 app.use(bodyParser);
 
-var secret = process.env.COOKIE_SECRET;
-
-app.use(cookieParser(secret));
-app.use(cookieSession({
-  keys: [secret],
-  domain: 'withregard.io'
-}));
-
-app.use(auth);
+app.use(auth(regardUserStore));
 app.use('/api', api);
 app.use('/portal', portal);
 app.use('/', website);

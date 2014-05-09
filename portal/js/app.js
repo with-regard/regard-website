ddd9683976e6c41e6e6e2bd9de58232fabc2197b
David Blurton <david.blurton@gmail.com>
2014-05-09 12:29:17 +0100

    Send cookies with AJAX requests

--- portal/js/app.js
+++ portal/js/app.js
@@ -5,7 +5,11 @@ var App = Ember.Application.create({
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
   host: 'https://website-api.withregard.io',
-  namespace: 'v1'
+  namespace: 'v1',
+  ajax: function(url, method, hash) {
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
+  }
 });
 
 App.ApplicationSerializer = DS.RESTSerializer.extend({

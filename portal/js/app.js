c8cb2dceb54bb6d5861923b4afb6f047cfc62595
David Blurton <david.blurton@gmail.com>
2014-05-09 15:05:56 +0100

    Hash could be undefined

--- portal/js/app.js
+++ portal/js/app.js
@@ -7,6 +7,7 @@ App.ApplicationAdapter = DS.RESTAdapter.extend({
   host: 'https://website-api.withregard.io',
   namespace: 'v1',
   ajax: function(url, method, hash) {
+    hash = hash || {};
     hash.xhrFields = {withCredentials: true};
     return this._super(url, method, hash);
   }

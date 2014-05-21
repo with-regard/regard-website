e80b643dbfa0d2f745a11b150339af1ab5448902
David Blurton <david.blurton@gmail.com>
2014-05-21 23:40:05 +0100

    Fixed bar height instead of scale to fit height

--- portal/js/components/BarChartComponent.js
+++ portal/js/components/BarChartComponent.js
@@ -1,50 +1,36 @@
 App.BarChartComponent = Ember.Component.extend({
   tagName: 'svg',
-  attributeBindings: 'width height'.w(),
-  margin: {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 20
-  },
-
+  attributeBindings: 'width'.w(),
+  barHeight: 20,
+  
   w: function () {
-    return this.get('width') - this.get('margin.left') - this.get('margin.right') - 243;
+    return this.get('width') - 200 - 20;
   }.property('width'),
 
-  h: function () {
-    return this.get('height') - this.get('margin.top') - this.get('margin.bottom');
-  }.property('height'),
-
   transformG: function () {
-    return "translate(" + (this.get('margin.left') + 243) + "," + this.get('margin.top') + ")";
+    return "translate(" + 200 + "," + "22" + ")";
   }.property(),
-
-  transformX: function () {
-    return "translate(0,0)"; // + this.get('h') + ")";
-  }.property('h'),
 
   draw: function () {
     var width = this.get('w');
-    var height = this.get('h');
     var data = this.get('data').sort(function (a, b) {
       return d3.descending(a.value, b.value);
     });
     var svg = d3.select('#' + this.get('elementId'));
     
     var x = d3.scale.linear().range([0, width]);
-    var y = d3.scale.ordinal().rangeRoundBands([1, height], 0.1);
+    var y = d3.scale.ordinal().rangeRoundBands([0, data.length * this.get('barHeight')], 0.1);
     
     var xAxis = d3.svg.axis().scale(x).orient("top");
     var yAxis = d3.svg.axis().scale(y).orient("left");
 
-    y.domain(data.map(function (d) {
-      return d.name;
-    }));
-
     x.domain([0, d3.max(data, function (d) {
       return d.value;
     })]);
+    
+    y.domain(data.map(function (d) {
+      return d.name;
+    }));
 
     svg.select(".axis.x").call(xAxis);
     svg.select(".axis.y").call(yAxis);
@@ -57,15 +43,15 @@ App.BarChartComponent = Ember.Component.extend({
       .attr("y", function (d) {
         return y(d.name);
       })
-      .attr("x", 1)
+      .attr("x", 0)
       .attr("height", y.rangeBand())
       .attr("width", 0)
       .transition()
       .delay(function (d, i) {
         return i * 20;
       }) // time between drawing bars
-    .duration(400) // time to grow the bar to full height
-    .attr("x", 1)
+    .duration(400) // time to grow the bar to full width
+    .attr("x", 0)
       .attr("width", function (d) {
         return x(d.value);
       });

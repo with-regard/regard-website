1a3928eb245e91c5d67d44bd0c3c9e6c61c18802
David Blurton <david.blurton@gmail.com>
2014-05-22 18:40:48 +0100

    Fix graphs

--- portal/js/components/BarChartComponent.js
+++ portal/js/components/BarChartComponent.js
@@ -2,7 +2,7 @@ App.BarChartComponent = Ember.Component.extend({
   tagName: 'svg',
   attributeBindings: 'width'.w(),
   barHeight: 20,
-  
+
   w: function () {
     return this.get('width') - 200 - 20;
   }.property('width'),
@@ -14,23 +14,22 @@ App.BarChartComponent = Ember.Component.extend({
   draw: function () {
     var width = this.get('w');
     var data = this.get('data').sort(function (a, b) {
-        return d3.descending(a.value, b.value);
+      return d3.descending(a.value, b.value);
     });
-    
-    console.log(data[0].value);
-    
+
     var svg = d3.select('#' + this.get('elementId'));
-    
+
     var x = d3.scale.linear().range([0, width]);
     var y = d3.scale.ordinal().rangeRoundBands([0, data.length * this.get('barHeight')], 0.1);
-    
+
     var xAxis = d3.svg.axis().scale(x).orient("top").tickFormat(d3.format("d")).tickSubdivide(0);
     var yAxis = d3.svg.axis().scale(y).orient("left");
-    
-    x.domain([0, d3.max(data, function (d) {
-      return d.value;
-    })]);
-    
+
+    var max = d3.max(data, function (d) {
+      return +d.value;
+    });
+
+    x.domain([0, max]);
     y.domain(data.map(function (d) {
       return d.name;
     }));

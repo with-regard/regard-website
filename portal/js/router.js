2b5decc44dbe57c6be905a3116aed9ee443267de
David Blurton <david.blurton@gmail.com>
2014-05-28 20:26:20 +0100

    Transition straight to chart if we find one.

--- portal/js/router.js
+++ portal/js/router.js
@@ -3,7 +3,7 @@ App.Router.map(function () {
     this.resource('user-welcome');
     this.resource('project', {path: '/:project_id'}, function() {
       this.resource('investigation', {path: '/:investigation_id'}, function() {
-        this.resource('chart', {path: '/:chart_id'}, function(){
+        this.resource('chart', {path: '/:chart_id'}, function() {
           this.route('edit');
         });
       });
@@ -18,13 +18,14 @@ App.Router.reopen({
 
 App.ApplicationRoute = Ember.Route.extend({
   model: function () {
-    var self = this;
-    
     return this.store.find('user').then(function(user) {
       return user.get('firstObject');
-    }, function(error){
-      console.log();
     });
+  },
+  actions: {
+    error: function(error, transition) {
+      console.log(error.message)
+    }
   }
 });
 
@@ -39,3 +40,20 @@ App.ProjectRoute = Ember.Route.extend({
     });
   }
 });
+
+App.InvestigationRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('investigation', params.investigation_id);    
+  },
+  afterModel: function(investigation) {
+    var self = this;
+    
+    investigation.get('charts').then(function(charts) {
+      var chart = charts.get('firstObject');
+      
+      if (chart) {
+        self.transitionTo('chart', chart);
+      }
+    })
+  }
+})

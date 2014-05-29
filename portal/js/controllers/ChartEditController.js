224c56ed7da455e9ec8105e6efd088a4cd8eade2
David Blurton <david.blurton@gmail.com>
2014-05-29 13:38:22 +0100

    Create a dummy chart data object so the chart data loads async

--- portal/js/controllers/ChartEditController.js
+++ portal/js/controllers/ChartEditController.js
@@ -3,7 +3,16 @@ App.ChartEditController = App.AuthenticationController.extend({
     registerQuery: function() {
       console.log('registering a query');
       var chart = this.get('model');
-      chart.save();
+
+      // Put a dummy chartdata in the chart to trigger the async loading of the chart data
+      // Since the request will be fulfilled by the query api, don't need to save the record. 
+      var chartdata = this.store.createRecord('chartdata');
+      chartdata.set('id', chart.id);
+      
+      chart.get('chartdata').then(function(chartdatas){
+        chartdatas.pushObject(chartdata);
+        chart.save();
+      })
     }
   }
 });

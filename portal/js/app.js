befa074ff4ddae9bed824dde1a1810aee44562f2
David Blurton <david.blurton@gmail.com>
2014-06-02 21:21:01 +0100

    Found some extra debug flags

--- portal/js/app.js
+++ portal/js/app.js
@@ -1,6 +1,10 @@
 var App = Ember.Application.create({
+  LOG_STACKTRACE_ON_DEPRECATION: true,
+  LOG_BINDINGS: true,
   LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_ACTIVE_GENERATION: true
 });
 
 Ember.Inflector.inflector.irregular('chartdata', 'chartdata');

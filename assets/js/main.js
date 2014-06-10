e8f9d625c7227781eff96d35e7d0d5cdef6f6125
eneiand <neil.t.anderson@gmail.com>
2014-06-10 22:15:55 +0100

    persist the session id

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,24 @@ $(document).foundation();
 
 requirejs.config({
     paths: {
-        'regard': 'https://github.com/with-regard/regard-js-client/releases/download/v0.2-alpha/regard'
+        'regard': 'https://github.com/with-regard/regard-js-client/releases/download/v0.3-alpha/regard'
     }
 });
 
 require(['regard'], function (regard) {
     var userId = localStorage.getItem('userId');
+    var sessionId = sessionStorage.getItem('sessionId');
 
     if (userId) {
         regard.setUserId(userId);
     } else {
         localStorage.setItem('userId', regard.getUserId());
+    }
+
+    if (sessionId) {
+        regard.setSessionId(sessionId);
+    } else {
+        localStorage.setItem(regard.getSessionId());
     }
 
     regard.setRegardURL('https://api.withregard.io/track/v1/WithRegard/Regard/event');

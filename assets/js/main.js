f66ecc0ac61fbd95035327986d5b2533e5bb462f
eneiand <neil.t.anderson@gmail.com>
2014-06-10 21:53:31 +0100

    add require.js, send a site.visit event to the WithRegard/Regard/event endpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+requirejs.config({
+    paths: {
+        'regard': 'https://github.com/with-regard/regard-js-client/releases/download/v0.2-alpha/regard'
+    }
+});
+
+require(['regard'], function (regard) {
+    var userId = localStorage.getItem('userId');
+
+    if (userId) {
+        regard.setUserId(userId);
+    } else {
+        localStorage.setItem('userId', regard.getUserId());
+    }
+
+    regard.setRegardURL('https://api.withregard.io/track/v1/WithRegard/Regard/event');
+
+    regard.trackEvent('page.visited', {
+        'page.url': document.URL
+    }).then(function (e) {
+        console.log(e)
+    });
+});

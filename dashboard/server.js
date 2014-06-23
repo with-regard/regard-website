c9a5961d2085b94ace19a35b19bc21865814110f
David Blurton <david.blurton@gmail.com>
2014-06-23 18:40:20 +0100

    Change server so it works with push state

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
-var dist = __dirname + '/dist';
 
-// Routes
-app.get('/dashboard', function (req, res, next) {
-  res.sendfile('index.html', { root: dist });
+app.use('/dashboard', express.static(__dirname + '/dist'));
+
+app.get('/dashboard/*', function (req, res, next) {
+  res.sendfile('index.html', { root: __dirname + '/dist' });
 });
-
-app.use(express.static(dist));
 
 module.exports = app;

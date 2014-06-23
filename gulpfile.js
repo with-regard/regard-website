688c40508a52c65b84aa3acea2f3fcc84e2eefe2
David Blurton <david.blurton@gmail.com>
2014-06-23 22:31:10 +0100

    Remove gulp asset pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,11 @@
 "use strict";
 
-// Gulp
 var gulp = require('gulp');
-
-// Plugins
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var prefix = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var livereload = require('gulp-livereload');
 var nodemon = require('gulp-nodemon');
-var path = require('path');
-var tinylr = require('tiny-lr');
-var WritableStream = require('stream').Writable;
-var svgSprites = require('gulp-svg-sprites');
-var svg = svgSprites.svg;
-var png = svgSprites.png;
 
 var outputDir = 'website/dist';
 
-// Paths
-var paths = {
-  scripts: ['assets/js/*.js'],
-  images: ['assets/img/**'],
-  icons: ['assets/icons/*.svg']
-};
-
-// Uglify JS
-gulp.task('uglify', function () {
-  return gulp.src(paths.scripts)
-    .pipe(plumber())
-    .pipe(uglify({
-      outSourceMap: false
-    }))
-    .pipe(gulp.dest(outputDir + '/assets/js'))
-});
-
-// Copy images.
-gulp.task('copyimages', function () {
-  return gulp.src(paths.images)
-    .pipe(gulp.dest(outputDir + '/assets/img'));
-});
-
-gulp.task('sprites', function () {
-  return gulp.src(paths.icons)
-    .pipe(svg({
-      className: ".%f-icon",
-      cssFile: "_sprites.scss"
-    }))
-    .pipe(gulp.dest('assets/scss'))
-    .pipe(png())
-});
-
-// Watch files
-gulp.task('watch', function (event) {
-  gulp.watch(paths.images, ['copyimages']);
-  gulp.watch(paths.icons, ['sprites']);
-  gulp.watch(paths.scripts, ['uglify']);
-});
-
-gulp.task('server', ['build'], function (cb) {
+gulp.task('server', function (cb) {
   var productConfig = require('./development-config.json');
 
   if (productConfig['port'] === -1) {
@@ -77,11 +22,4 @@ gulp.task('server', ['build'], function (cb) {
     });
 });
 
-gulp.task('azure-exit', ['build'], function (cb) {
-	process.exit(0);
-	cb(err);
-});
-
-gulp.task('build', ['uglify', 'copyimages']);
-gulp.task('azure', ['azure-exit']);
-gulp.task('default', ['sprites', 'server', 'watch']); //there is a race condition here so it will probably fail the first time
+gulp.task('default', ['server']); //there is a race condition here so it will probably fail the first time

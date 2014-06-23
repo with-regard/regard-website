e09fcd6398251e83c293d90a02c6aa7a55e0d097
David Blurton <david.blurton@gmail.com>
2014-06-23 16:21:04 +0100

    Delete old portal

--- portal/js/models/ProjectModel.js
+++
@@ -1,5 +0,0 @@
-App.Project = DS.Model.extend({
-  name: DS.attr('string', {defaultValue: 'New project'}),
-  investigations: DS.hasMany('investigation', {async: true}),
-  user: DS.belongsTo('user')
-});

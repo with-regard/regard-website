61c5fe992f53151622a0b84114902a0f2a40d8e9
David Blurton <david.blurton@gmail.com>
2014-07-02 12:10:09 +0100

    Fix module resolver

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'dashboard', // TODO: loaded via config
+  modulePrefix: 'app', // TODO: loaded via config
   Resolver: Resolver
 });
 
-loadInitializers(App, 'dashboard');
+loadInitializers(App, 'app');
 
 export default App;

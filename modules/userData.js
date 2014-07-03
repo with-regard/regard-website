3bc1fb837f7a546d4569e3101c2df19ab6675983
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-03 13:58:36 +0100

    update link

--- modules/userData.js
+++ modules/userData.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/userdata/:organization/:product/:user', function (req, res, next) {
-  res.redirect(['/portal#', 'userevents', req.params.organization, req.params.product, req.params.user].join('/'));
+  res.redirect(['/dashboard/#', 'userevents', req.params.organization, req.params.product, req.params.user].join('/'));
 });
 
 module.exports = router;

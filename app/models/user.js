300c9670216c8af6898de6dd32f9eeea0e26c72e
David Blurton <david.blurton@gmail.com>
2014-07-07 09:46:12 +0100

    Replace project controller with organization

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  projects: DS.hasMany('project', {async: true}),
   isDeveloper: DS.attr('boolean'),
   isUser: DS.attr('boolean'),
   isAnonymous: DS.attr('boolean'),
--- app/router.js
+++ app/router.js
@@ -6,10 +6,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('userevents', {path: '/userevents/:organization/:product/:userevent_id'});
+  this.route('user-welcome');
 
-  this.resource('projects', {path: '/project'}, function() {
-    this.resource('user-welcome');
-
+  this.resource('organization', {path: '/:organization_id'}, function() {
     this.resource('project', {path: '/:project_id'}, function() {
       this.resource('investigation', {path: '/:investigation_id'}, function() {
         this.route('new');

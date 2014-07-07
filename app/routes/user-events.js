a89caa05001e74bf7706f1d55bdbbd1be9b0b9fd
David Blurton <david.blurton@gmail.com>
2014-07-07 11:06:50 +0100

    Missing a dash here

--- app/routes/user-events.js
+++ app/routes/user-events.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function(params) {
 
     // Create an adapter that overrides find or buildUrl.
-    return this.store.find('userevent', [params.organization, params.product, params.userevent_id].join('/')).then(function(model){
+    return this.store.find('user-event', [params.organization, params.product, params.userevent_id].join('/')).then(function(model){
       model.set('organization', params.organization);
       model.set('product', params.product);
 

4ef4ec779cf132ce737281bf92d3225f551f909d
David Blurton <david.blurton@gmail.com>
2014-07-07 13:54:51 +0100

    Use environment variable for website api url
    Conflicts:
    	app/adapters/application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'https://website-api.withregard.io',
+  host: DashboardENV.WEBSITE_API_URL,
   namespace: 'v1',
   ajax: function(url, method, hash) {
     hash = hash || {};

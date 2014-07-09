269b4e18ee41152a197350f5db2af4d1c04bb934
David Blurton <david.blurton@gmail.com>
2014-07-09 17:26:37 +0100

    Create organization in mongo

--- app/controllers/setup.js
+++ app/controllers/setup.js
@@ -1,38 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['application'],
+
   actions: {
     createOrganization: function() {
+      var self = this;
+
       var organizationName = this.get('organization');
-      var productName = this.get('product');
-      var userId; // get userId
+      var projectName = this.get('project');
+      var userId = this.get('controllers.application.content.id');
 
-      // validate organzation and product names (convert to lowercase and dasherize)
-
-      // create organization
       var organization = this.store.createRecord('organization', {
-        name: organizationName
-        users: [userId];
+        name: organizationName,
+        users: [userId]
       });
 
       var project = this.store.createRecord('project',  {
-        name: productName
+        name: projectName
       })
 
       project.save().then(function() {
         organization.get('projects').then(function(projects){
           projects.pushObject(project);
-        })
 
-        organization.save().then(function() {
-          this.set('organizationId', organization.get('id'));
-          this.transitionToRoute('setup.install-client');
+          organization.save().then(function() {
+            self.set('organization', organization);
+            self.set('project', project);
+            self.transitionToRoute('setup.install-client');
+          });
         });
       });
     },
 
     goToProject: function() {
-      var organizationId = this.get('organizationId');
+      var organizationId = this.get('organization.id');
       this.transistionToRoute('organization', organizationId);
     }
   }
--- app/models/organization.js
+++ app/models/organization.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  projects: DS.hasMany('project', {async: true})
+  projects: DS.hasMany('project', {async: true}),
+  users: DS.attr()
 });

0b7313bfe1acff5650d0ff394fd52a1d4bf8f31a
David Blurton <david.blurton@gmail.com>
2014-07-16 11:47:55 +0100

    Redirect broken

--- modules/userData.js
+++ modules/userData.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/userdata/:organization/:project/:user', function (req, res, next) {
-  res.redirect(['/dashboard/', 'userevents', req.params.organization, req.params.project, req.params.user].join('/'));
+  res.redirect(['/dashboard', 'userevents', req.params.organization, req.params.project, req.params.user].join('/'));
 });
 
 module.exports = router;

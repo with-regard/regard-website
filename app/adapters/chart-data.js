a5399bd0c89d2eb53aef832d618c6eb29da0e170
David Blurton <david.blurton@gmail.com>
2014-07-16 14:52:06 +0100

    If there isn't an value, use event count
    Can’t currently write a query that shows event count

--- app/adapters/chart-data.js
+++ app/adapters/chart-data.js
@@ -5,7 +5,12 @@ export default Ember.Object.extend({
   find: function(organizationId, projectId, chartId) {
     var url = [DashboardENV.WEBSITE_DATASTORE_URL, 'v1', 'chartdata', organizationId, projectId, chartId].join('/');
     return ajax(url).then(function(data) {
-      return data;
+      return data.map(function(x) {
+        if(!x.hasOwnProperty('value')) {
+          x.value = x.EventCount;
+        }
+        return x;
+      });
     });
   }
 });

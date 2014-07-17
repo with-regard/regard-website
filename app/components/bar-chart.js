6af185826558bd9d2d24704515816c78c5a94dd8
David Blurton <david.blurton@gmail.com>
2014-07-17 16:13:21 +0100

    Update points if we can, rather than redrawing the whole chart

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -3,15 +3,34 @@ import charts from 'regard-highcharts';
 
 export default Ember.Component.extend({
   draw: function (data, xAxisLabel, yAxisLabel) {
-    var chartData = charts.bar(data, xAxisLabel, yAxisLabel);
-    this.$().highcharts(chartData);
-  },
-
-  update: function () {
     var data = this.get('data');
     var xAxisLabel = this.get('xAxisLabel');
     var yAxisLabel = this.get('yAxisLabel');
 
-    this.draw(data, xAxisLabel, yAxisLabel);
+    var chartData = charts.bar(data, xAxisLabel, yAxisLabel);
+    chartData.chart.renderTo = this.$()[0];
+    var chart = new Highcharts.Chart(chartData);
+
+    this.set('chart', chart);
+  },
+
+  redraw: function(data) {
+    var chart = this.get('chart');
+
+    if(chart && chart.series[0].data.length === data.length) {
+      var newPoints = data.map(x => x.value);
+
+      $.each(chart.series[0].data, function (i, point) {
+        point.update(newPoints[i], false);
+      });
+
+      chart.redraw();
+    } else {
+      this.draw();
+    }
+  },
+
+  update: function () {
+    this.redraw();
   }.observes('data')
 });

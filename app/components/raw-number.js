e5cf297d398c6d29b57676f51a1e81b0fffc34d9
David Blurton <david.blurton@gmail.com>
2014-07-17 17:41:28 +0100

    Pointless spinning
    I’m so sorry

--- app/components/raw-number.js
+++ app/components/raw-number.js
@@ -6,8 +6,30 @@ function numberWithCommas(x) {
 
 export default Ember.Component.extend({
   value: 0,
+  spin: function() {
+    var steps = 20;
+    var totalTime = 500;
+
+    var targetValue = this.get('targetValue');
+
+    for (var i = 0; i <= steps; i++) {
+      (function(num) {
+        return function() {
+          Ember.run.later(this, function() {
+            var intermediateValue = Math.floor(targetValue / steps * num);
+            this.set('value', numberWithCommas(intermediateValue));
+          }, totalTime / steps * num);
+        }
+      })(i).call(this);
+    }
+  },
 
   update: function() {
-    this.set('value', numberWithCommas(this.get('data.firstObject.value')));
+    var newValue = this.get('data.firstObject.value');
+
+    if(this.get('targetValue') !== newValue){
+      this.set('targetValue', newValue);
+      this.spin();
+    }
   }.observes('data')
 });

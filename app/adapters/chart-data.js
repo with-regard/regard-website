254f7a882e29dafbd3123d65b8732fee13b5c1cc
David Blurton <david.blurton@gmail.com>
2014-07-17 11:38:38 +0100

    Rename data store to query api

--- app/adapters/chart-data.js
+++ app/adapters/chart-data.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 
 export default Ember.Object.extend({
   find: function(organizationId, projectId, chartId) {
-    var url = [DashboardENV.WEBSITE_DATASTORE_URL, 'v1', 'chartdata', organizationId, projectId, chartId].join('/');
+    var url = [DashboardENV.WEBSITE_QUERY_API_URL, 'v1', 'chartdata', organizationId, projectId, chartId].join('/');
     return ajax(url).then(function(data) {
       return data.map(function(x) {
         if(!x.hasOwnProperty('value')) {
--- app/adapters/user-data.js
+++ app/adapters/user-data.js
@@ -3,12 +3,12 @@ import Ember from "ember";
 
 export default Ember.Object.extend({
   find: function(organizationId, projectId, chartId) {
-    var url = [DashboardENV.WEBSITE_DATASTORE_URL, 'v1', 'userevents', organizationId, projectId, chartId].join('/');
+    var url = [DashboardENV.WEBSITE_QUERY_API_URL, 'v1', 'userevents', organizationId, projectId, chartId].join('/');
     return ajax(url);
   },
 
   deleteData: function(organizationId, projectId, chartId) {
-    var url = [DashboardENV.WEBSITE_DATASTORE_URL, 'v1', 'userevents', organizationId, projectId, chartId, 'delete-data'].join('/');
+    var url = [DashboardENV.WEBSITE_QUERY_API_URL, 'v1', 'userevents', organizationId, projectId, chartId, 'delete-data'].join('/');
     return $.delete(url);
   }
 });
